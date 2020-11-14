a1d33136f66dbe0dc5ede668f6758ef085099eb9
schuelermine <git@anselmschueler.com>
2020-11-14 14:53:04 +0100

    things

--- toggleCrazy.js
+++
@@ -1,7 +0,0 @@
-function toggleCrazy() {
-    if (document.getElementById("html").classList.contains("crazy")) {
-        document.getElementById("html").classList.remove("crazy");
-    } else {
-        document.getElementById("html").classList.add("crazy");
-    }
-}
