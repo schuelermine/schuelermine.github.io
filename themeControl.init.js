1e08e54fd06a73914a4611ec29cbc7a39ed63929
Anselm Schüler <git@anselmschueler.com>
2021-01-03 21:17:12 +0100

    REVAMP EVERYTHING

--- themeControl.init.js
+++ themeControl.init.js
@@ -1,15 +1,4 @@
-// Themes
-
-const themes = {
-    "dark-theme": ["#141414", "white", "yellow"],
-    "light-theme": ["white", "#141414", "crimson"]
+function setTheme(theme) {
+    document.getElementById("theme").href = theme;
 }
-
-// Initialize theme
-
-var savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-    html.style.setProperty("--background-color", themes[savedTheme][0]);
-    html.style.setProperty("--text-color", themes[savedTheme][1]);
-    html.style.setProperty("--link-color", themes[savedTheme][2]);
-}
+setTheme(localStorage.getItem("theme") ?? "theme/auto.css");
--- themeControl.js
+++ themeControl.js
@@ -1,27 +1,9 @@
-// Elements
-
-const html = document.getElementById("html")
-const checkbox = document.getElementById("remember-theme-check")
-
-// Set and unset themes
-
-function setTheme(ω) {
-    html.style.setProperty("--background-color", themes[ω][0]);
-    html.style.setProperty("--text-color", themes[ω][1]);
-    html.style.setProperty("--link-color", themes[ω][2]);
-    if (checkbox.checked) {
-        window.localStorage.setItem("theme", ω);
-        checkbox.checked = false;
+function saveTheme(theme) {
+    if (document.getElementById("save-theme").checked) {
+        localStorage.setItem("theme", theme);
     }
 }
-
-function unsetTheme() {
-    html.style.removeProperty("--background-color");
-    html.style.removeProperty("--text-color");
-    html.style.removeProperty("--link-color");
-
-    if (checkbox.checked) {
-        window.localStorage.removeItem("theme");
-        checkbox.checked = false;
-    }
+function setAndSaveTheme(theme) {
+    setTheme(theme);
+    saveTheme(theme);
 }
