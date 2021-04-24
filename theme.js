2fa829896a1e83fde1367becd134eca9ce97f9fe
Anselm Schüler <git@anselmschueler.com>
2021-04-24 03:40:40 +0200

    It WORKS!

--- theme.js
+++ theme.js
@@ -1,38 +1,45 @@
 "use strict";
-var knownThemes = ["dark", "light", "auto"];
+var knownThemes = ['dark', 'light', 'auto'];
 var themeTable = {
-    "dark": "",
-    "light": "",
-    "auto": ""
+    'dark': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;}',
+    'light': ':root {--background-color: white; --text-color: #141414; --link-color: crimson;}',
+    'auto': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}'
 };
 function isKnownTheme(name) {
     return knownThemes.includes(name);
 }
 function isTheme(obj) {
-    return obj.type === "namedTheme" && isKnownTheme(obj.value)
-        || obj.type === "customTheme";
+    return obj.type === 'namedTheme' && isKnownTheme(obj.value)
+        || obj.type === 'customTheme';
 }
-function getTheme() {
-    var savedTheme = localStorage.getItem("theme");
+function NamedTheme(name) {
+    return { type: 'namedTheme', value: name };
+}
+function CustomTheme(str) {
+    return { type: 'customTheme', value: str };
+}
+function getSavedTheme() {
+    var savedTheme = localStorage.getItem('theme');
     var theme;
     if (savedTheme === null) {
-        theme = { type: "namedTheme", value: "auto" };
+        theme = { type: 'namedTheme', value: 'auto' };
     }
     else {
         try {
             var obj = JSON.parse(savedTheme);
-            theme = isTheme(obj) ? obj : { type: "namedTheme", value: "auto" };
+            theme = isTheme(obj) ? obj : { type: 'namedTheme', value: 'auto' };
         }
         catch (_a) {
-            theme = { type: "namedTheme", value: isKnownTheme(savedTheme) ? savedTheme : "auto" };
+            theme = { type: 'namedTheme', value: isKnownTheme(savedTheme) ? savedTheme : 'auto' };
         }
     }
     return theme;
 }
-function applyTheme(theme) {
-    var themeStr = theme.type === "namedTheme" ? theme.value : themeTable[theme.value];
-    var themeNode = document.getElementById("theme");
+function setTheme(theme) {
+    var themeStr = theme.type === 'namedTheme' ? themeTable[theme.value] : theme.value;
+    var themeNode = document.getElementById('theme');
     if (themeNode !== null) {
         themeNode.innerHTML = themeStr;
     }
 }
+setTheme(getSavedTheme());
--- theme.ts
+++ theme.ts
@@ -1,47 +1,51 @@
-const knownThemes = ["dark", "light", "auto"];
+const knownThemes = ['dark', 'light', 'auto'];
 let themeTable: {[name: string]: string} = {
-    "dark": "",
-    "light": "",
-    "auto": ""
+    'dark': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;}',
+    'light': ':root {--background-color: white; --text-color: #141414; --link-color: crimson;}',
+    'auto': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}'
 }
 
 type Theme = NamedTheme | CustomTheme;
-type NamedTheme = {type: "namedTheme", value: (typeof knownThemes)[number]};
-type CustomTheme = {type: "customTheme", value: string};
-    //                                               ______
-    // Unfortunately, you can't do string => CSSStyleSheet w/o dummy objects,
-    // and you can't set an HTMLStyleElement's content to anything except a string.
-    // It works, but it's non-self-documenting and feels kinda iffy
+type NamedTheme = {type: 'namedTheme', value: (typeof knownThemes)[number]};
+type CustomTheme = {type: 'customTheme', value: string};
 
-function isKnownTheme(name: string): name is NamedTheme["value"] {
+function isKnownTheme(name: string): name is NamedTheme['value'] {
     return knownThemes.includes(name);
 }
-
 function isTheme(obj: any): obj is Theme {
-    return obj.type === "namedTheme" && isKnownTheme(obj.value)
-        || obj.type === "customTheme"
+    return obj.type === 'namedTheme' && isKnownTheme(obj.value)
+        || obj.type === 'customTheme'
 }
 
-function getTheme(): Theme {
-    let savedTheme: string | null = localStorage.getItem("theme");
+function NamedTheme(name: NamedTheme['type']): NamedTheme {
+    return {type: 'namedTheme', value: name}
+}
+function CustomTheme(str: string): CustomTheme {
+    return {type: 'customTheme', value: str}
+}
+
+function getSavedTheme(): Theme {
+    let savedTheme: string | null = localStorage.getItem('theme');
     let theme: Theme;
     if (savedTheme === null) {
-        theme = {type: "namedTheme", value: "auto"};
+        theme = {type: 'namedTheme', value: 'auto'};
     } else {
         try {
             let obj = JSON.parse(savedTheme)
-            theme = isTheme(obj) ? obj : {type: "namedTheme", value: "auto"};
+            theme = isTheme(obj) ? obj : {type: 'namedTheme', value: 'auto'};
         } catch {
-            theme = {type: "namedTheme", value: isKnownTheme(savedTheme) ? savedTheme : "auto"};
+            theme = {type: 'namedTheme', value: isKnownTheme(savedTheme) ? savedTheme : 'auto'};
         }
     }
     return theme;
 }
 
-function applyTheme(theme: Theme): void {
-    let themeStr = theme.type === "namedTheme" ? theme.value : themeTable[theme.value];
-    let themeNode = document.getElementById("theme");
+function setTheme(theme: Theme): void {
+    let themeStr = theme.type === 'namedTheme' ? themeTable[theme.value] : theme.value;
+    let themeNode = document.getElementById('theme');
     if (themeNode !== null) {
         themeNode.innerHTML = themeStr
     }
 }
+
+setTheme(getSavedTheme());
