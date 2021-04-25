c43e2a3515f2eeedc4a5d1a796091dc56264c9ad
Anselm Schüler <git@anselmschueler.com>
2021-04-26 00:58:20 +0200

    YESSSSSS!

--- theme.js
+++ theme.js
@@ -1,45 +1,57 @@
 "use strict";
-var knownThemes = ['dark', 'light', 'auto'];
+var knownThemes = ["dark", "light", "auto"];
 var themeTable = {
-    'dark': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;}',
-    'light': ':root {--background-color: white; --text-color: #141414; --link-color: crimson;}',
-    'auto': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}'
+    "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
+    "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
+    "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
 };
 function isKnownTheme(name) {
     return knownThemes.includes(name);
 }
 function isTheme(obj) {
-    return obj.type === 'namedTheme' && isKnownTheme(obj.value)
-        || obj.type === 'customTheme';
+    return obj.type === "namedTheme" && isKnownTheme(obj.value)
+        || obj.type === "customTheme";
 }
 function NamedTheme(name) {
-    return { type: 'namedTheme', value: name };
+    return { type: "namedTheme", value: name };
 }
 function CustomTheme(str) {
-    return { type: 'customTheme', value: str };
+    return { type: "customTheme", value: str };
 }
 function getSavedTheme() {
-    var savedTheme = localStorage.getItem('theme');
+    var savedTheme = localStorage.getItem("theme");
     var theme;
     if (savedTheme === null) {
-        theme = { type: 'namedTheme', value: 'auto' };
+        theme = { type: "namedTheme", value: "auto" };
     }
     else {
         try {
             var obj = JSON.parse(savedTheme);
-            theme = isTheme(obj) ? obj : { type: 'namedTheme', value: 'auto' };
+            theme = isTheme(obj) ? obj : { type: "namedTheme", value: "auto" };
         }
         catch (_a) {
-            theme = { type: 'namedTheme', value: isKnownTheme(savedTheme) ? savedTheme : 'auto' };
+            theme = { type: "namedTheme", value: isKnownTheme(savedTheme) ? savedTheme : "auto" };
         }
     }
     return theme;
 }
-function setTheme(theme) {
-    var themeStr = theme.type === 'namedTheme' ? themeTable[theme.value] : theme.value;
-    var themeNode = document.getElementById('theme');
+function applyTheme(theme) {
+    var themeStr = theme.type === "namedTheme" ? themeTable[theme.value] : theme.value;
+    var themeNode = document.getElementById("theme");
     if (themeNode !== null) {
         themeNode.innerHTML = themeStr;
     }
 }
-setTheme(getSavedTheme());
+function trySaveTheme(theme) {
+    var checkbox = document.getElementById("save-theme");
+    if (checkbox.checked) {
+        localStorage.setItem("theme", JSON.stringify(theme));
+    }
+    checkbox.checked = false;
+}
+function buttonHandler(name) {
+    var theme = NamedTheme(name);
+    applyTheme(theme);
+    trySaveTheme(theme);
+}
+applyTheme(getSavedTheme());
--- theme.ts
+++ theme.ts
@@ -1,51 +1,65 @@
-const knownThemes = ['dark', 'light', 'auto'];
+const knownThemes = ["dark", "light", "auto"];
 let themeTable: {[name: string]: string} = {
-    'dark': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;}',
-    'light': ':root {--background-color: white; --text-color: #141414; --link-color: crimson;}',
-    'auto': ':root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}'
+    "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
+    "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
+    "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
 }
 
 type Theme = NamedTheme | CustomTheme;
-type NamedTheme = {type: 'namedTheme', value: (typeof knownThemes)[number]};
-type CustomTheme = {type: 'customTheme', value: string};
+type NamedTheme = {type: "namedTheme", value: "dark" | "light" | "auto"};
+type CustomTheme = {type: "customTheme", value: string};
 
-function isKnownTheme(name: string): name is NamedTheme['value'] {
+function isKnownTheme(name: string): name is NamedTheme["value"] {
     return knownThemes.includes(name);
 }
 function isTheme(obj: any): obj is Theme {
-    return obj.type === 'namedTheme' && isKnownTheme(obj.value)
-        || obj.type === 'customTheme'
+    return obj.type === "namedTheme" && isKnownTheme(obj.value)
+        || obj.type === "customTheme"
 }
 
-function NamedTheme(name: NamedTheme['type']): NamedTheme {
-    return {type: 'namedTheme', value: name}
+function NamedTheme(name: NamedTheme["value"]): NamedTheme {
+    return {type: "namedTheme", value: name}
 }
 function CustomTheme(str: string): CustomTheme {
-    return {type: 'customTheme', value: str}
+    return {type: "customTheme", value: str}
 }
 
 function getSavedTheme(): Theme {
-    let savedTheme: string | null = localStorage.getItem('theme');
+    let savedTheme: string | null = localStorage.getItem("theme");
     let theme: Theme;
     if (savedTheme === null) {
-        theme = {type: 'namedTheme', value: 'auto'};
+        theme = {type: "namedTheme", value: "auto"};
     } else {
         try {
             let obj = JSON.parse(savedTheme)
-            theme = isTheme(obj) ? obj : {type: 'namedTheme', value: 'auto'};
+            theme = isTheme(obj) ? obj : {type: "namedTheme", value: "auto"};
         } catch {
-            theme = {type: 'namedTheme', value: isKnownTheme(savedTheme) ? savedTheme : 'auto'};
+            theme = {type: "namedTheme", value: isKnownTheme(savedTheme) ? savedTheme : "auto"};
         }
     }
     return theme;
 }
 
-function setTheme(theme: Theme): void {
-    let themeStr = theme.type === 'namedTheme' ? themeTable[theme.value] : theme.value;
-    let themeNode = document.getElementById('theme');
+function applyTheme(theme: Theme): void {
+    let themeStr = theme.type === "namedTheme" ? themeTable[theme.value] : theme.value;
+    let themeNode = document.getElementById("theme");
     if (themeNode !== null) {
-        themeNode.innerHTML = themeStr
+        themeNode.innerHTML = themeStr;
     }
 }
 
-setTheme(getSavedTheme());
+function trySaveTheme(theme: Theme) {
+    let checkbox = document.getElementById("save-theme") as HTMLInputElement;
+    if (checkbox.checked) {
+        localStorage.setItem("theme", JSON.stringify(theme));
+    }
+    checkbox.checked = false;
+}
+
+function buttonHandler(name: NamedTheme["value"]): void {
+    let theme = NamedTheme(name);
+    applyTheme(theme);
+    trySaveTheme(theme);
+}
+
+applyTheme(getSavedTheme());
