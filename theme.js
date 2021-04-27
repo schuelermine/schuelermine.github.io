c3a8547a56f77039269c48cf66ed464fd1e0c25c
Anselm Schüler <git@anselmschueler.com>
2021-04-27 03:32:37 +0200

    Theme system now saves currently selected theme temporarily in session storage.

--- theme.js
+++ theme.js
@@ -1,16 +1,11 @@
 "use strict";
 var knownThemes = ["dark", "light", "auto"];
-var themeTable = {
-    "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
-    "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
-    "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
-};
 function isKnownTheme(name) {
     return knownThemes.includes(name);
 }
 function isTheme(obj) {
     return obj.type === "namedTheme" && isKnownTheme(obj.value)
-        || obj.type === "customTheme";
+        || obj.type === "customTheme" && "value" in obj;
 }
 function NamedTheme(name) {
     return { type: "namedTheme", value: name };
@@ -18,22 +13,36 @@ function NamedTheme(name) {
 function CustomTheme(str) {
     return { type: "customTheme", value: str };
 }
-function getSavedTheme() {
-    var savedTheme = localStorage.getItem("theme");
+var defaultTheme = NamedTheme("auto");
+function parseTheme(str) {
     var theme;
-    if (savedTheme === null) {
-        theme = { type: "namedTheme", value: "auto" };
+    try {
+        var obj = JSON.parse(str);
+        theme = isTheme(obj) ? obj : defaultTheme;
     }
-    else {
-        try {
-            var obj = JSON.parse(savedTheme);
-            theme = isTheme(obj) ? obj : { type: "namedTheme", value: "auto" };
-        }
-        catch (_a) {
-            theme = { type: "namedTheme", value: isKnownTheme(savedTheme) ? savedTheme : "auto" };
-        }
+    catch (_a) {
+        theme = { type: "namedTheme", value: isKnownTheme(str) ? str : "auto" };
     }
     return theme;
+}
+var themeTable = {
+    "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
+    "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
+    "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
+};
+function getThemePreference() {
+    return localStorage.getItem("theme");
+}
+function getCurrentTheme() {
+    return sessionStorage.getItem("theme");
+}
+function getTheme() {
+    var _a;
+    var themeStr = (_a = getCurrentTheme()) !== null && _a !== void 0 ? _a : getThemePreference();
+    if (themeStr === null) {
+        return defaultTheme;
+    }
+    return parseTheme(themeStr);
 }
 function applyTheme(theme) {
     var themeStr = theme.type === "namedTheme" ? themeTable[theme.value] : theme.value;
@@ -42,16 +51,29 @@ function applyTheme(theme) {
         themeNode.innerHTML = themeStr;
     }
 }
-function trySaveTheme(theme) {
+function setThemePreference(str) {
+    localStorage.setItem("theme", str);
+}
+function setCurrentTheme(str) {
+    sessionStorage.setItem("theme", str);
+}
+function checkboxHandler() {
     var checkbox = document.getElementById("save-theme");
-    if (checkbox.checked) {
-        localStorage.setItem("theme", JSON.stringify(theme));
-    }
+    var wasChecked = checkbox.checked;
     checkbox.checked = false;
+    return wasChecked;
+}
+function saveThemeHandler(theme) {
+    var themeStr = JSON.stringify(theme);
+    var checked = checkboxHandler();
+    if (checked) {
+        setThemePreference(themeStr);
+    }
+    setCurrentTheme(themeStr);
 }
 function buttonHandler(name) {
     var theme = NamedTheme(name);
     applyTheme(theme);
-    trySaveTheme(theme);
+    saveThemeHandler(theme);
 }
-applyTheme(getSavedTheme());
+applyTheme(getTheme());
