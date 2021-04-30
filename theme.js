c72a9ac922ca49f6ca0866ff776af71e864beaf2
Anselm Schüler <git@anselmschueler.com>
2021-04-30 16:35:26 +0200

    Change comments

--- theme.js
+++ theme.js
@@ -1,19 +1,26 @@
 "use strict";
+/** Known theme names. */
 var knownThemes = ["dark", "light", "auto"];
+/** Checks if theme name is known. */
 function isKnownTheme(name) {
     return knownThemes.includes(name);
 }
+/** Checks for theme object structure. */
 function isTheme(obj) {
     return obj.type === "namedTheme" && isKnownTheme(obj.value)
         || obj.type === "customTheme" && "value" in obj;
 }
+/** Gets NamedTheme object from name. */
 function NamedTheme(name) {
     return { type: "namedTheme", value: name };
 }
+/** Gets CustomTheme object from CSS code. */
 function CustomTheme(str) {
     return { type: "customTheme", value: str };
 }
+/** Default/fallback theme for the website. */
 var defaultTheme = NamedTheme("auto");
+/** Gets theme object from string, with compatibility for naked theme names. */
 function parseTheme(str) {
     var theme;
     try {
@@ -25,17 +32,21 @@ function parseTheme(str) {
     }
     return theme;
 }
+/** Map from theme name to CSS code for all known named themes. */
 var themeTable = {
     "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
     "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
     "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
 };
+/** Gets persistent saved theme preference as string. */
 function getThemePreference() {
     return localStorage.getItem("theme");
 }
+/** Gets currently active theme from session storage as string. */
 function getCurrentTheme() {
     return sessionStorage.getItem("theme");
 }
+/** Gets currently active theme as object, falls back to theme preference. */
 function getTheme() {
     var _a;
     var themeStr = (_a = getCurrentTheme()) !== null && _a !== void 0 ? _a : getThemePreference();
@@ -44,6 +55,7 @@ function getTheme() {
     }
     return parseTheme(themeStr);
 }
+/** Applies theme to website by changing content of a style element. */
 function applyTheme(theme) {
     var themeCss = theme.type === "namedTheme" ? themeTable[theme.value] : theme.value;
     var themeNode = document.getElementById("theme");
@@ -51,18 +63,22 @@ function applyTheme(theme) {
         themeNode.innerHTML = themeCss;
     }
 }
+/** Sets persistent theme preference in local storage. */
 function setThemePreference(str) {
     localStorage.setItem("theme", str);
 }
+/** Sets currently active theme in session storage. */
 function setCurrentTheme(str) {
     sessionStorage.setItem("theme", str);
 }
+/** Gets checkbox state, sets it to unchecked, and returns the original state. */
 function checkboxHandler() {
     var checkbox = document.getElementById("save-theme");
     var wasChecked = checkbox.checked;
     checkbox.checked = false;
     return wasChecked;
 }
+/** Sets current theme, theme preference if needed, and resets checkbox. */
 function saveThemeHandler(theme) {
     var themeStr = JSON.stringify(theme);
     var checked = checkboxHandler();
@@ -71,9 +87,14 @@ function saveThemeHandler(theme) {
     }
     setCurrentTheme(themeStr);
 }
+/**
+ * Applies named theme with specified name, saves it, and resets the checkbox.
+ * Intended to be called by theme control buttons via onclick event.
+ */
 function buttonHandler(name) {
     var theme = NamedTheme(name);
     applyTheme(theme);
     saveThemeHandler(theme);
 }
+// Initialize theme on load.
 applyTheme(getTheme());
--- theme.ts
+++ theme.ts
@@ -1,60 +1,40 @@
-/**
- * Known theme names.
- */
+/** Known theme names. */
 const knownThemes = ["dark", "light", "auto"] as const;
 
-/**
- * Represents any theme object.
- */
+/** Represents any theme object. */
 type Theme = NamedTheme | CustomTheme;
 
-/**
- * A theme specified by name.
- */
+/** A theme specified by name. */
 type NamedTheme = {type: "namedTheme", value: (typeof knownThemes)[number]};
 
-/**
- * A theme specified by CSS code.
- */
+/** A theme specified by CSS code. */
 type CustomTheme = {type: "customTheme", value: string};
 
-/**
- * Checks if theme name is known.
- */
+/** Checks if theme name is known. */
 function isKnownTheme(name: string): name is NamedTheme["value"] {
     return knownThemes.includes(name as NamedTheme["value"]);
 }
 
-/**
- * Checks for theme object structure.
- */
+/** Checks for theme object structure. */
 function isTheme(obj: any): obj is Theme {
     return obj.type === "namedTheme" && isKnownTheme(obj.value)
         || obj.type === "customTheme" && "value" in obj;
 }
 
-/**
- * Gets NamedTheme object from name.
- */
+/** Gets NamedTheme object from name. */
 function NamedTheme(name: NamedTheme["value"]): NamedTheme {
     return {type: "namedTheme", value: name};
 }
 
-/**
- * Gets CustomTheme object from CSS code.
- */
+/** Gets CustomTheme object from CSS code. */
 function CustomTheme(str: string): CustomTheme {
     return {type: "customTheme", value: str};
 }
 
-/**
- * Default/fallback theme for the website.
- */
+/** Default/fallback theme for the website. */
 let defaultTheme = NamedTheme("auto")
 
-/**
- * Gets theme object from string, with compatibility for naked theme names.
- */
+/** Gets theme object from string, with compatibility for naked theme names. */
 function parseTheme(str: string): Theme {
     let theme: Theme;
     try {
@@ -66,32 +46,24 @@ function parseTheme(str: string): Theme {
     return theme;
 }
 
-/**
- * Map from theme name to CSS code for all known named themes.
- */
+/** Map from theme name to CSS code for all known named themes. */
 let themeTable: {[name: string]: string} = {
     "dark": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;}",
     "light": ":root {--background-color: white; --text-color: #141414; --link-color: crimson;}",
     "auto": ":root {--background-color: #141414; --text-color: white; --link-color: yellow;} @media (prefers-color-scheme: light) {:root {--background-color: white; --text-color: #141414; --link-color: crimson;}}"
 };
 
-/**
- * Gets persistent saved theme preference as string.
- */
+/** Gets persistent saved theme preference as string. */
 function getThemePreference(): string | null {
     return localStorage.getItem("theme");
 }
 
-/**
- * Gets currently active theme from session storage as string.
- */
+/** Gets currently active theme from session storage as string. */
 function getCurrentTheme(): string | null {
     return sessionStorage.getItem("theme");
 }
 
-/**
- * Gets currently active theme as object, falls back to theme preference.
- */
+/** Gets currently active theme as object, falls back to theme preference. */
 function getTheme(): Theme {
     let themeStr: string | null = getCurrentTheme() ?? getThemePreference();
     if (themeStr === null) {
@@ -100,9 +72,7 @@ function getTheme(): Theme {
     return parseTheme(themeStr);
 }
 
-/**
- * Applies theme to website by changing content of a style element.
- */
+/** Applies theme to website by changing content of a style element. */
 function applyTheme(theme: Theme): void {
     let themeCss: string = theme.type === "namedTheme" ? themeTable[theme.value] : theme.value;
     let themeNode: HTMLElement | null = document.getElementById("theme");
@@ -111,23 +81,17 @@ function applyTheme(theme: Theme): void {
     }
 }
 
-/**
- * Sets persistent theme preference in local storage.
- */
+/** Sets persistent theme preference in local storage. */
 function setThemePreference(str: string): void {
     localStorage.setItem("theme", str);
 }
 
-/**
- * Sets currently active theme in session storage.
- */
+/** Sets currently active theme in session storage. */
 function setCurrentTheme(str: string): void {
     sessionStorage.setItem("theme", str);
 }
 
-/**
- * Gets checkbox state, sets it to unchecked, and returns the original state.
- */
+/** Gets checkbox state, sets it to unchecked, and returns the original state. */
 function checkboxHandler(): boolean {
     let checkbox: HTMLInputElement = document.getElementById("save-theme") as HTMLInputElement;
     let wasChecked: boolean = checkbox.checked
@@ -135,9 +99,7 @@ function checkboxHandler(): boolean {
     return wasChecked
 }
 
-/**
- * Sets current theme, theme preference if needed, and resets checkbox.
- */
+/** Sets current theme, theme preference if needed, and resets checkbox. */
 function saveThemeHandler(theme: Theme): void {
     let themeStr: string = JSON.stringify(theme);
     let checked: boolean = checkboxHandler();
