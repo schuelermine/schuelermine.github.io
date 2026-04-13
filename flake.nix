e784bb7526d153d9fa70933ad74ec3be02bc8dbe
Anselm Schüler <mail@anselmschueler.com>
2026-04-13 23:34:47 +0200

    flake.nix, flake.lock: update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
